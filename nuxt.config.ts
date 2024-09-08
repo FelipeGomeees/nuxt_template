4451151e5c477b53c30ade2fbc35907178759331
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-09-08 17:00:58 -0300

    build: setup vueify file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['vuetify-nuxt-module'],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: './vuetify.config.js'
+  },
 })
