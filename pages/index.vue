46343bf87419d0039c577e15f6af0117533e3c8d
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-09-08 20:49:43 -0300

    cleanup: body reset code it was a problem with v-container, not related with body reset

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,3 @@
       </NuxtLayout>
     </div>
 </template>
-
-<script>
-useHead({
-  bodyAttrs: {
-    class: 'body--reset'
-  }
-})
-</script>
-
-<style>
-    body--reset {
-        margin: 0;
-        padding: 0;
-    }
-</style>
